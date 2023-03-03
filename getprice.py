af1c14745cb95ab7fcb27da8658fc9efadd9d05b
Kanav Jain <jainkanav25@yahoo.com>
2023-03-02 23:40:00 -0800

    takes input, bug fix

--- getprice.py
+++ getprice.py
@@ -1,17 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
+def getData (symbol):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
+    url = 'https://finance.yahoo.com/quote/{}/'.format(symbol)
+    r = requests.get(url, headers = headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-url = 'https://finance.yahoo.com/quote/AMZN/'
+    price = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('fin-streamer')[0].text
+    change = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('fin-streamer')[1].text
+    percentage = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('fin-streamer')[2].text
 
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
-price = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
+    stock = {
+        'symbol' : symbol,
+        'Price' : price,
+        'Change' : change,
+        'percentage' : percentage
+    }
+    
+    return stock
 
-print(price)
 
-
-
-
-
+name = input("Enter Stock Symbol: ")
+print("Stock: ", name)
+print(getData(name))
