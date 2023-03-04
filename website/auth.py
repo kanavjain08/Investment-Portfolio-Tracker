5b9236b8894e0ec8ab7c2cf779402dceac75f49e
Kanav Jain <jainkanav25@yahoo.com>
2023-03-03 22:02:47 -0800

    message flashes, get and post from server

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
     return render_template("login.html", boolean = True)
 
@@ -10,6 +10,24 @@ def login():
 def logout():
     return "hello"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
-    return render_template("sign_up.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password = request.form.get('password')
+        stockSymbol = request.form.get('stockSymbol')
+
+        if len(email) < 2:
+            flash('Email invalid', category = 'error')
+        elif len(firstName) < 2:
+            flash('Name invalid', category = 'error')
+        elif len(stockSymbol) < 3:
+            flash('Invalid', category = 'error')
+        else:
+            #add user to database 
+            flash('Account created!', category = 'success')
+    else:
+        pass
+
+    return render_template("sign_up.html") 
