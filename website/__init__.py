80e4be085cbdbc73106cb6ca167ef42e4ba57046
Kanav Jain <jainkanav25@yahoo.com>
2023-03-05 18:13:51 -0800

    database creation

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
 
     # enrypts/secures cookies of session data
     app.config['SECRET_KEY'] = 'kj'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -12,4 +19,14 @@ def create_app():
     app.register_blueprint(views, url_prefix = '/')
     app.register_blueprint(auth, url_prefix = '/')
 
+    from .models import User, Note
+
+    with app.app_context():
+        create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists ('website/' + DB_NAME):
+        db.create_all()
+        print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template, request, flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", boolean = True)
 
 @auth.route('/logout')
@@ -16,18 +20,15 @@ def sign_up():
         email = request.form.get('email')
         firstName = request.form.get('firstName')
         password = request.form.get('password')
-        stockSymbol = request.form.get('stockSymbol')
-
+        stocksymbol = request.form.get('stocksymbol')
         if len(email) < 2:
             flash('Email invalid', category = 'error')
         elif len(firstName) < 2:
             flash('Name invalid', category = 'error')
-        elif len(stockSymbol) < 3:
-            flash('Invalid', category = 'error')
+        elif len(stocksymbol) <2:
+            flash('Stock invalid', category = 'error')
         else:
-            #add user to database 
+            new_user = User(email = email, firstName = firstName)
             flash('Account created!', category = 'success')
-    else:
-        pass
-
-    return render_template("sign_up.html") 
+    return render_template("sign_up.html")
+    
