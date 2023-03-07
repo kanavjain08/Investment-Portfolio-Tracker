dc0f2b4dac225e7aba385992518f13de6099a003
Kanav Jain <jainkanav25@yahoo.com>
2023-03-06 22:41:41 -0800

    fixed database bug, database complete. Login/sign up complete.

--- website/views.py
+++ website/views.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/home')
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user = current_user)
+
+@views.route('/')
+@login_required
+def home_def():
+    return render_template("home.html", user = current_user)
+
