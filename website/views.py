3b6ee9253e28d75579e19bb124bd9fb94ed93cde
Kanav Jain <jainkanav25@yahoo.com>
2023-03-10 01:12:12 -0800

    displays value that only corresponds to the user, stored in db, displays the list

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from getprice import *
+from .models import Stock
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -10,8 +12,10 @@ def home():
     if request.method == 'POST':
         symbol = request.form.get('stocksymbol')
         stock_info = getData(symbol)
-        name = "TSLA"
-        _data = getData(name)
+        stock = request.form.get('stock')
+        new_stock = Stock(data = stock, user_id = current_user.id)
+        db.session.add(new_stock)
+        db.session.commit()
         return render_template("home.html", user = current_user, name = symbol, _data = stock_info)
     else:
         return render_template("home.html", user = current_user)
@@ -23,8 +27,9 @@ def home_def():
     if request.method == 'POST':
         symbol = request.form.get('stocksymbol')
         stock_info = getData(symbol)
-        name = "TSLA"
-        _data = getData(name)
+        new_stock = Stock(data = str(stock_info), user_id = current_user.id)
+        db.session.add(new_stock)
+        db.session.commit()
         return render_template("home.html", user = current_user, name = symbol, _data = stock_info)
     else:
         return render_template("home.html", user = current_user)
